2bc0f4a8ad6ee5bf485c158f5b2099ea810e814b
fernando.cervantes <fernando.cervantes@oliverwyman.com>
2020-12-17 12:56:09 -0600

    test

---
+++ NodesManipulation.groovy
@@ -0,0 +1,42 @@
+import org.apache.jackrabbit.oak.spi.commit.CommitInfo
+import org.apache.jackrabbit.oak.spi.commit.EmptyHook
+import org.apache.jackrabbit.oak.spi.state.NodeStore
+import org.apache.jackrabbit.oak.commons.PathUtils
+
+def rmNode(def session, String path) {
+    println "Removing node ${path}"
+
+    NodeStore ns = session.store
+    def nb = ns.root.builder()
+
+    def aBuilder = nb
+    for(p in PathUtils.elements(path)) {  aBuilder = aBuilder.getChildNode(p) }
+
+    if(aBuilder.exists()) {
+        rm = aBuilder.remove()
+        ns.merge(nb, EmptyHook.INSTANCE, CommitInfo.EMPTY)
+        return rm
+    } else {
+        println "Node ${path} doesn't exist"
+        return false
+    }
+}
+
+def addNode(def session, String path, Strin parentPath) {
+    println "Removing node ${path}"
+
+    NodeStore ns = session.store
+    def nb = ns.root.builder()
+
+    def aBuilder = nb
+    for(p in PathUtils.elements(path)) {  aBuilder = aBuilder.getChildNode(parentPath) }
+
+    if(aBuilder.exists()) {
+        rm = aBuilder.setChildNode(path)
+        ns.merge(nb, EmptyHook.INSTANCE, CommitInfo.EMPTY)
+        return rm
+    } else {
+        println "Node ${path} doesn't exist"
+        return false
+    }
+}
